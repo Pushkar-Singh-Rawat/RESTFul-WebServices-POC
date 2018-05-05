c917b44e6729ca1920a7cd87f9791117f9fddbdd
desiCoda <j.pushkarsingh@gmail.com>
2018-05-05 19:32:27 +0530

    added customer domain class

---
+++ src/main/java/demo/domain/Customer.java
@@ -0,0 +1,19 @@
+package demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Customer {
+private String firstname;
+private String lastname;
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+	
+}
