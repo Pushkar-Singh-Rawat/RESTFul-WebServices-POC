57780f4ebf356514f463f2ca0a5e9387c43eef73
desiCoda <j.pushkarsingh@gmail.com>
2018-05-05 21:45:00 +0530

    updated Customer repository

--- src/main/java/demo/repositories/CustomerRepository.java
+++ src/main/java/demo/repositories/CustomerRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import demo.domain.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	public Customer findById(Long Id);
 
 }
