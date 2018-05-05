ada624c6605c4c743c8d927a1ed5267505d85bbb
desiCoda <j.pushkarsingh@gmail.com>
2018-05-05 21:43:16 +0530

    updated CustomerDTO data transfer object

--- src/main/java/demo/api/v1/model/CustomerDTO.java
+++ src/main/java/demo/api/v1/model/CustomerDTO.java
@@ -1,10 +1,15 @@
 package demo.api.v1.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
 	private String firstname;
 	private String lastname;
-	private Long id;
+	private String customerURL;
+	private Long Id;
 }
