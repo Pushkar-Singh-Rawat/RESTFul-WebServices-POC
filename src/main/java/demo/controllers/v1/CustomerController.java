6f012cd4d054efb698f9a80fb5334f99ec66706a
desiCoda <j.pushkarsingh@gmail.com>
2018-05-05 21:44:29 +0530

    updated getMappings for CustomerController.java

--- src/main/java/demo/controllers/v1/CustomerController.java
+++ src/main/java/demo/controllers/v1/CustomerController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import demo.api.v1.model.CustomerDTO;
@@ -11,7 +13,7 @@ import demo.api.v1.model.CustomerDTOList;
 import demo.services.CustomerService;
 
 @Controller
-@RequestMapping("/api/v1/customers")
+@RequestMapping("/api/v1/customers/")
 public class CustomerController {
 
 	private CustomerService customerService;
@@ -23,5 +25,17 @@ public class CustomerController {
 		return new ResponseEntity<CustomerDTOList>(
 				new CustomerDTOList(customerService.getAllCustomers())
 				, HttpStatus.OK);
+	}
+	
+	@GetMapping({"{Id}"})
+	public ResponseEntity<CustomerDTO> getAllCustomers(@PathVariable String Id){
+		return new ResponseEntity<CustomerDTO>(
+				customerService.getCustomerByID(Long.valueOf(Id))
+				, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public void createNewCustomer(){
+		
 	}
 }
