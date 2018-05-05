945fad878dfa81d887f5cdff1109c55d896c2822
desiCoda <j.pushkarsingh@gmail.com>
2018-05-05 19:30:52 +0530

    added CustomerController

---
+++ src/main/java/demo/controllers/v1/CustomerController.java
@@ -0,0 +1,27 @@
+package demo.controllers.v1;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import demo.api.v1.model.CustomerDTO;
+import demo.api.v1.model.CustomerDTOList;
+import demo.services.CustomerService;
+
+@Controller
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+	private CustomerService customerService;
+	public CustomerController(CustomerService customerService) {
+		this.customerService=customerService;
+	}
+	@GetMapping
+	public ResponseEntity<CustomerDTOList> getAllCustomers(){
+		return new ResponseEntity<CustomerDTOList>(
+				new CustomerDTOList(customerService.getAllCustomers())
+				, HttpStatus.OK);
+	}
+}
