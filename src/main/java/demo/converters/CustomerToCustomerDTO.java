fdba2817108c842967515777fe6f5836ea00d28b
desiCoda <j.pushkarsingh@gmail.com>
2018-05-05 19:31:59 +0530

    added converter for Customer model and domain classes

---
+++ src/main/java/demo/converters/CustomerToCustomerDTO.java
@@ -0,0 +1,24 @@
+package demo.converters;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import demo.api.v1.model.CustomerDTO;
+import demo.domain.Customer;
+import lombok.Synchronized;
+@Component
+public class CustomerToCustomerDTO implements Converter<Customer,CustomerDTO>{
+
+	@Synchronized
+	@Override
+	public CustomerDTO convert(Customer customer) {
+		if(customer!=null){
+			CustomerDTO customerDTO=new CustomerDTO();
+			customerDTO.setFirstname(customer.getFirstname());
+			customerDTO.setLastname(customer.getLastname());
+			customerDTO.setId(customer.getId());
+			return customerDTO;
+		}
+		return null;
+	}
+
+}
