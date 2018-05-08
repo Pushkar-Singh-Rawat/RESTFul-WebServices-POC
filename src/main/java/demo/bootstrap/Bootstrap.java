b49031d74f6f98d3d0982c4b12a810675e934a5f
desiCoda <j.pushkarsingh@gmail.com>
2018-05-08 12:13:19 +0530

    refactored Bootstrap class. added saveCategories and saveCustomers methods

--- src/main/java/demo/bootstrap/Bootstrap.java
+++ src/main/java/demo/bootstrap/Bootstrap.java
@@ -12,66 +12,65 @@ import demo.repositories.CategoryRepository;
 import demo.repositories.CustomerRepository;
 
 @Component
-public class Bootstrap implements CommandLineRunner{
+public class Bootstrap implements CommandLineRunner {
 
 	private CategoryRepository categoryRepository;
 	private CustomerRepository customerRepository;
-	public Bootstrap(CategoryRepository categoryRepository,CustomerRepository customerRepository){
-		this.categoryRepository=categoryRepository;
-		this.customerRepository=customerRepository;
+
+	public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+		this.categoryRepository = categoryRepository;
+		this.customerRepository = customerRepository;
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		Category fruits=new Category();
+	public void saveCatrgories() {
+		Category fruits = new Category();
 		fruits.setName("Fruits");
-		Category dried=new Category();
+		Category dried = new Category();
 		dried.setName("Dried");
-		Category fresh=new Category();
+		Category fresh = new Category();
 		fresh.setName("Fresh");
-		Category exotic=new Category();
+		Category exotic = new Category();
 		exotic.setName("Exotic");
-		Category nuts=new Category();
+		Category nuts = new Category();
 		nuts.setName("Nuts");
-		Customer customer1=new Customer();
-		customer1.setFirstname("Peter");
-		customer1.setLastname("parker");
-		
-		Customer customer2=new Customer();
-		customer2.setFirstname("Tony");
-		customer2.setLastname("Stark");
-		
-		Customer customer3=new Customer();
-		customer3.setFirstname("Thor");
-		customer3.setLastname("Odinson");
-		
-		Customer customer4=new Customer();
-		customer4.setFirstname("Wanda");
-		customer4.setLastname("Maximoff");
-		
-		/*List<Category> categories=new ArrayList<>();
-		categories.add(fruits);
-		categories.add(nuts);
-		categories.add(dried);
-		categories.add(fresh);
-		categories.add(exotic);
-		categoryRepository.save(categories);*/
-		
 		categoryRepository.save(fruits);
 		categoryRepository.save(dried);
 		categoryRepository.save(nuts);
 		categoryRepository.save(fresh);
 		categoryRepository.save(exotic);
+		System.out.println("category data loaded +++++++" + categoryRepository.count());
+
+	}
+
+	public void saveCutomers() {
+
+		Customer customer1 = new Customer();
+		customer1.setFirstname("Peter");
+		customer1.setLastname("parker");
+
+		Customer customer2 = new Customer();
+		customer2.setFirstname("Tony");
+		customer2.setLastname("Stark");
+
+		Customer customer3 = new Customer();
+		customer3.setFirstname("Thor");
+		customer3.setLastname("Odinson");
+
+		Customer customer4 = new Customer();
+		customer4.setFirstname("Wanda");
+		customer4.setLastname("Maximoff");
 		customerRepository.save(customer1);
 		customerRepository.save(customer2);
 		customerRepository.save(customer3);
 		customerRepository.save(customer4);
-		System.out.println("category data loaded +++++++"+categoryRepository.count());
-		System.out.println("customer data loaded +++++++"+customerRepository.count());
-		
-		
-		
+		System.out.println("customer data loaded +++++++" + customerRepository.count());
 	}
-	
+
+	@Override
+	public void run(String... args) throws Exception {
+		saveCatrgories();
+		saveCutomers();
+
+	}
+
 }
