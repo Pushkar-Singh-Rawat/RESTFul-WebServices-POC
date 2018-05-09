c675f7f0d7588e0b1c1300783836742f0a5386a1
desiCoda <j.pushkarsingh@gmail.com>
2018-05-09 13:51:13 +0530

    updated CategoryDTO,CustomerDTO model classes. Added swagger api annotations

--- src/main/java/demo/api/v1/model/CategoryDTO.java
+++ src/main/java/demo/api/v1/model/CategoryDTO.java
@@ -1,11 +1,14 @@
 package demo.api.v1.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class CategoryDTO {
 
+	
 	private Long id;
+	@ApiModelProperty(value="This is name of the category",required=true)
 	private String name;
 	
 	/*public CategoryDTO(){
